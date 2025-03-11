35bed6c57ec98245c78572741d079c485d72b4e4
abhinasregmi <aviregmi2018@gmail.com>
2025-03-11 22:27:16 +0545

    chore: prisma setup
    Setup prisma ORM for postgresql. Created configuration files to manage env variables.

---
+++ be/src/common/env.ts
@@ -0,0 +1,9 @@
+export function env<T>(name: string, value: T = undefined){
+  const envValue = process.env[name] ?? value;
+  
+  if(!envValue){
+    throw new Error(`Cannot resolve the value for config ${name}`);
+  }
+  
+  return envValue;
+}
