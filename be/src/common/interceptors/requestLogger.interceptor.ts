5729bcc6a6c8ae62f2a8caeac361f365c741a0dc
abhinasregmi <aviregmi2018@gmail.com>
2025-03-12 09:31:51 +0545

    feat:be-docs Enable request logging and documentation with swagger

---
+++ be/src/common/interceptors/requestLogger.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class RequestLog implements NestInterceptor {
+  private readonly logger = new Logger(RequestLog.name);
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+
+    const { ip, method, path: url } = request;
+    const controller = context.getClass();
+    const handler = context.getHandler();
+
+    this.logger.log(`${ip} ${method} ${url} ${controller} ${handler}`);
+
+    return next.handle();
+  }
+}
