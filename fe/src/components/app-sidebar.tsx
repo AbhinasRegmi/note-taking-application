dcfe1d34c2924e15d8431ba591ccc92be1d5ab6d
abhinasregmi <aviregmi2018@gmail.com>
2025-03-14 19:16:45 +0545

    feat: create sidebar

---
+++ fe/src/components/app-sidebar.tsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import { Bookmark, Lightbulb } from "lucide-react";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Note Taking Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <NavLink to="/">
+                    <Lightbulb />
+                    <span>Notes</span>
+                  </NavLink>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <NavLink to="/tags">
+                    <Bookmark />
+                    <span>Tags</span>
+                  </NavLink>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
--- fe/src/layouts/base.tsx
+++ fe/src/layouts/base.tsx
@@ -1,9 +1,15 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Outlet } from "react-router";
 
 export function BaseLayout() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <SidebarProvider defaultOpen={true}>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <Outlet />
+      </main>
+    </SidebarProvider>
   );
 }
