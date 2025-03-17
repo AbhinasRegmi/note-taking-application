292d13ef52d73eb0f114598c282f060f767d81f3
abhinasregmi <aviregmi2018@gmail.com>
2025-03-17 20:44:09 +0545

    chore: update component path

--- fe/src/layouts/base.tsx
+++ fe/src/layouts/base.tsx
@@ -1,11 +1,11 @@
 import { AppNavbar } from "@/components/bars/app-navbar";
-import { AppSidebar } from "@/components/app-sidebar";
 import { CategoriesProvider } from "@/providers/category";
 import { RefetchProvider } from "@/providers/notes";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AuthProvider } from "@/providers/auth";
 import { ThemeProvider } from "@/providers/theme";
 import { Outlet } from "react-router";
+import { AppSidebar } from "@/components/bars/app-sidebar";
 
 export function BaseLayout() {
   return (
